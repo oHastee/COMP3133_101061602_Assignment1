93bc5d49845ee0fd1148bc70358daaf197ec8694
oHastee <oscarpiediaz@gmail.com>
2025-04-01 16:07:19 -0400

    Edited routes for new edit and add components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
-import { EmployeeFormComponent } from './employee/employee-form/employee-form.component';
+import { EmployeeAddComponent } from './employee/employee-add/employee-add.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 export const appRoutes: Routes = [
@@ -11,7 +12,7 @@ export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'employees', component: EmployeeListComponent, canActivate: [AuthGuard] },
-  { path: 'employee/add', component: EmployeeFormComponent, canActivate: [AuthGuard] },
-  { path: 'employee/edit/:id', component: EmployeeFormComponent, canActivate: [AuthGuard] },
+  { path: 'employee/add', component: EmployeeAddComponent, canActivate: [AuthGuard]  },
+  { path: 'employee/edit/:id', component: EmployeeEditComponent, canActivate: [AuthGuard]  },
  // { path: '**', redirectTo: 'login' },
 ];
