aa65f8fcfa11a035191660a028f3734ce4a37364
oHastee <oscarpiediaz@gmail.com>
2025-04-03 13:47:43 -0400

    created page header

---
+++ frontend/src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,18 @@
+// src/app/shared/components/page-header/page-header.component.ts
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent {
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() backLink: string = '';
+  @Input() backText: string = 'Back';
+}
