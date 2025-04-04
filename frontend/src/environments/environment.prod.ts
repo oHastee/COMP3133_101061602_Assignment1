275d95e150aac4b162a37c56549dbe6f8e5e0aea
oHastee <oscarpiediaz@gmail.com>
2025-04-04 18:46:52 -0400

    Updated for production

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// frontend/src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://employee-management-api-19ru.onrender.com',
+  graphqlUrl: 'https://employee-management-api-19ru.onrender.com/graphql',
+  uploadsUrl: 'https://employee-management-api-19ru.onrender.com/uploads',
+};
