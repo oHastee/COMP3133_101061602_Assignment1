f8b7f8c4257ae382113cdd0fda33cd3d2310fa62
oHastee <oscarpiediaz@gmail.com>
2025-04-06 11:09:25 -0400

    Updated for production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://employee-management-api-19ru.onrender.com',
-  graphqlUri: 'https://employee-management-api-19ru.onrender.com/graphql'
+  graphqlUrl: 'https://employee-management-api-19ru.onrender.com/graphql'
 };
